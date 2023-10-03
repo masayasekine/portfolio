5b038174a18a9e364644cb33d325f0e060a53421
masayasekine <negirider250@gmail.com>
2023-10-03 22:46:05 +0900

    <wip> about page

--- mysite/views.py
+++ mysite/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request: object) -> object:
     return render(request, "index.html")
+
+
+def about(request: object) -> object:
+    return render(request, "about.html")
